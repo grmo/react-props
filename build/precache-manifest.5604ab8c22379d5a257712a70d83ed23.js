bec4af4e398a851887393fa6aba5eeb31ec8fd2d
grmo <43529985+grmo@users.noreply.github.com>
2020-12-18 18:56:53 +0000

    Add the props to the card

---
+++ build/precache-manifest.5604ab8c22379d5a257712a70d83ed23.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff035fd7a70b3b0cca88f622354a5a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b834a920de7d1609468",
+    "url": "/static/js/2.2d8dda31.chunk.js"
+  },
+  {
+    "revision": "7215b949af3af2ba205f",
+    "url": "/static/js/main.77f9a277.chunk.js"
+  },
+  {
+    "revision": "e08213e0835c2085c902",
+    "url": "/static/js/runtime-main.552c1ce9.js"
+  }
+]);
